a2ce48e858f1a1898450a2e457838b90fc8cf0eb
DavideFarci <yank92.df@gmail.com>
2023-06-26 18:44:55 +0200

    add bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,8 @@
     <div class="container">
         <div class="row row-cols-3">
             @foreach ($movies as $movie)
-                <div class="col py-3 h-100">
-                    <div class="card">
-                        @include('partials.card')
-                    </div>
+                <div class="col py-3 h-100"> 
+                    @include('partials.card')
                 </div>
             @endforeach
         </div>
--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card">
+{{-- <div class="card">
     <img class="h-100" src="{{ Vite::asset('public/images/' . $movie->image) }}" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
@@ -8,4 +8,28 @@
         <p class="card-text">Voto: {{ $movie->vote}}</p>
         <a href="#" class="card-link">Go to Movie</a>
     </div>
+</div> --}}
+
+{{-- <div class="card">
+    <img class="h-100" src="{{ Vite::asset('public/images/' . $movie->image) }}" alt="...">
+    <div class="card__content">
+      <p class="card__title">Card Title
+      </p><p class="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+</div> --}}
+
+<div class="flip-card">
+    <div class="flip-card-inner">
+        <div class="flip-card-front">
+            <img class="h-100" src="{{ Vite::asset('public/images/' . $movie->image) }}" class="card-img-top" alt="...">
+        </div>
+        <div class="flip-card-back">
+            <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}</h6>
+            <p class="card-text">Nazionalità: {{ $movie->nationality}}</p>
+            <p class="card-text">Data: {{ $movie->date}}</p>
+            <p class="card-text">Voto: {{ $movie->vote}}</p>
+        </div>
+    </div>
 </div>
+  
