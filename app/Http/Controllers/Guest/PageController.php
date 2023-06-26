effa538d0e9aef60265242d8dda9e04a8804eb2e
DavideFarci <yank92.df@gmail.com>
2023-06-26 15:56:30 +0200

    set PageController and index() function

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Guest;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('welcome');
+    }
 }
