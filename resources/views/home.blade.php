e7e916d5464b7c869d0852886986944989166080
DavideFarci <yank92.df@gmail.com>
2023-06-26 17:19:51 +0200

    add base layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel Model Controller</title>
-        @vite('resources/js/app.js')
-    </head>
+@extends('layout.base')
 
-
-    <body>
-        <div class="container">
-            <div class="row row-cols-3">
-                @foreach ($movies as $movie)
-                    <div class="col">
-                        <div class="card">
-                            {{-- <img src="..." class="card-img-top" alt="..."> --}}
-                            <div class="card-body">
-                                <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}</h6>
-                                <p class="card-text">Nazionalità: {{ $movie->nationality}}</p>
-                                <p class="card-text">Data: {{ $movie->date}}</p>
-                                <p class="card-text">Voto: {{ $movie->vote}}</p>
-                                <a href="#" class="card-link">Go to Movie</a>
-                            </div>
-                          </div>
+@section('content')
+    <div class="container">
+        <div class="row row-cols-3">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card">
+                        {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                        @include('partials.card')
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
-    </body>
-</html>
+    </div>
+@endsection
